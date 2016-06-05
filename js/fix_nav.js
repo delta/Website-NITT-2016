d4694a9ac761d61294c77ab995e0b1e873158959
Sachin <ediblepeople@gmail.com>
2016-06-05 15:53:57 +0530

    Fix vertical scrolling bug. Now nav bar aligns properly on load.

--- js/fix_nav.js
+++ js/fix_nav.js
@@ -1,33 +1,57 @@
-$(document).ready(function() {
-$(document).scroll(function scroll_fix_nav	() {
-	
-  if(($(window).width()) >=1001){
-  if(window.scrollY >= 143)
-  	{
-  		if($('[id="k-menu affix"]').attr('class') == 'k-main-navig')
-  		{$('[id="k-menu affix"]').toggleClass('k-main-navig navbar-fixed-top');
-    	 
-  			$('.navbar-fixed-top').css("background-color" , 'rgba(255, 255, 255, 1)');
-    	}
-
-    }  
-  else{
-  	if($('[id="k-menu affix"]').attr('class') == 'navbar-fixed-top')
-  	{$('[id="k-menu affix"]').toggleClass('navbar-fixed-top k-main-navig');
-	 $('.k-main-navig').css("background-color" , '#FFFFFF')
-	 }
-		
-	
-  }
-
-}
-});
-// else {
-// 	$(".k-main-navig").css("visibility", "hidden");
-// }
-if(($(window).width()) >=1001)
+$(document).ready(function()
 {
-  $('.search-icon').css('margin','0');
-}
+	if (($(window).width()) >= 1001)
+	{
+		if (window.scrollY >= 143)
+		{
+			if ($('[id="k-menu affix"]').attr('class') == 'k-main-navig')
+			{
+				$('[id="k-menu affix"]').toggleClass('k-main-navig navbar-fixed-top');
 
+				$('.navbar-fixed-top').css("background-color", 'rgba(255, 255, 255, 1)');
+			}
+
+		}
+		else
+		{
+			if ($('[id="k-menu affix"]').attr('class') == 'navbar-fixed-top')
+			{
+				$('[id="k-menu affix"]').toggleClass('navbar-fixed-top k-main-navig');
+				$('.k-main-navig').css("background-color", '#FFFFFF')
+			}
+
+
+		}
+		$('.search-icon').css('margin', '0');
+
+	}
+
+});
+$(document).scroll(function scroll_fix_nav()
+{
+
+	if (($(window).width()) >= 1001)
+	{
+		if (window.scrollY >= 143)
+		{
+			if ($('[id="k-menu affix"]').attr('class') == 'k-main-navig')
+			{
+				$('[id="k-menu affix"]').toggleClass('k-main-navig navbar-fixed-top');
+
+				$('.navbar-fixed-top').css("background-color", 'rgba(255, 255, 255, 1)');
+			}
+
+		}
+		else
+		{
+			if ($('[id="k-menu affix"]').attr('class') == 'navbar-fixed-top')
+			{
+				$('[id="k-menu affix"]').toggleClass('navbar-fixed-top k-main-navig');
+				$('.k-main-navig').css("background-color", '#FFFFFF')
+			}
+
+
+		}
+
+	}
 });
