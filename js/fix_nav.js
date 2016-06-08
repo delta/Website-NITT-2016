62a8f7b2287dc6a840c1f3993b06a278864a3fab
shravan97 <shravanmurali@gmail.com>
2016-06-08 18:17:25 +0530

    small fix to navbar issues

--- js/fix_nav.js
+++ js/fix_nav.js
@@ -1,35 +1,40 @@
-$(document).ready(function()
-{
-	$(document).scroll(function scroll_fix_nav()
-{
+$(document).ready(function() {
+$(document).scroll(function scroll_fix_nav	() {
+  // var is_small_res = ( $( '.visible-xs' ).css( 'display' ) === 'block' );
+  var is_small_res = ($(window).width() <= 1040);
+ if(!is_small_res){ 
+  var headerHeight = $('header').outerHeight(true)-$('[id="k-menu affix"]').outerHeight();
+  if(window.scrollY >= headerHeight)
+  	{
+  		if($('[id="k-menu affix"]').attr('class') == 'k-main-navig')
+  		{$('[id="k-menu affix"]').toggleClass('k-main-navig navbar-fixed-top');
+    	 
+  			$('.navbar-fixed-top').css("background-color" , 'rgba(255, 255, 255, 1)');
+    	}
 
-	if (($(window).width()) >= 1001)
-	{
-		if (window.scrollY >= 143)
-		{
-			if ($('[id="k-menu affix"]').attr('class') == 'k-main-navig')
-			{
-				$('[id="k-menu affix"]').toggleClass('k-main-navig navbar-fixed-top');
+    }  
+  else{
+  	if($('[id="k-menu affix"]').attr('class') == 'navbar-fixed-top')
+  	{$('[id="k-menu affix"]').toggleClass('navbar-fixed-top k-main-navig');
+	 $('.k-main-navig').css("background-color" , '#FFFFFF')
+	 }
+		
+	
+  }
 
-				$('.navbar-fixed-top').css("background-color", 'rgba(255, 255, 255, 1)');
-			}
-
-		}
-		else
-		{
-			if ($('[id="k-menu affix"]').attr('class') == 'navbar-fixed-top')
-			{
-				$('[id="k-menu affix"]').toggleClass('navbar-fixed-top k-main-navig');
-				$('.k-main-navig').css("background-color", '#FFFFFF')
-			}
-
-
-		}
-
-	}
+}
 });
 
-	if (($(window).width()) >= 1001)
-	$('.search-icon').css('margin', '0');
+$('.search-icon').css('margin', '0');
+
+// else {
+// 	$(".k-main-navig").css("visibility", "hidden");
+// }
+
+if($('#menubar > ul').attr('class')!='sideMenu')
+$('#menubar > ul').attr('class' , 'sideMenu');
+
+if($('.goog-te-menu-value'))
+$('.goog-te-menu-value > span').html('Translate');
 
 });
