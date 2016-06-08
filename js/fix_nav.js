26dccfb5bf804c04374c07be7f573ef9f6c4aaf2
shravan97 <shravanmurali@gmail.com>
2016-06-08 18:04:08 +0530

    small js fix

--- js/fix_nav.js
+++ js/fix_nav.js
@@ -1,33 +1,6 @@
 $(document).ready(function()
 {
-	if (($(window).width()) >= 1001)
-	{
-		if (window.scrollY >= 143)
-		{
-			if ($('[id="k-menu affix"]').attr('class') == 'k-main-navig')
-			{
-				$('[id="k-menu affix"]').toggleClass('k-main-navig navbar-fixed-top');
-
-				$('.navbar-fixed-top').css("background-color", 'rgba(255, 255, 255, 1)');
-			}
-
-		}
-		else
-		{
-			if ($('[id="k-menu affix"]').attr('class') == 'navbar-fixed-top')
-			{
-				$('[id="k-menu affix"]').toggleClass('navbar-fixed-top k-main-navig');
-				$('.k-main-navig').css("background-color", '#FFFFFF')
-			}
-
-
-		}
-		$('.search-icon').css('margin', '0');
-
-	}
-
-});
-$(document).scroll(function scroll_fix_nav()
+	$(document).scroll(function scroll_fix_nav()
 {
 
 	if (($(window).width()) >= 1001)
@@ -54,4 +27,9 @@ $(document).scroll(function scroll_fix_nav()
 		}
 
 	}
+});
+
+	if (($(window).width()) >= 1001)
+	$('.search-icon').css('margin', '0');
+
 });
